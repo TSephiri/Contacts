654ed71f66814f6124b85b20533e11dc3b82c23b
Tshweu <t.t.sephiri@gmail.com>
2019-11-14 03:16:45 +0200

    created add contact method

--- NodeJS/contact.js
+++ NodeJS/contact.js
@@ -5,9 +5,35 @@ const bodyParser = require('body-parser')
 const crypto = require('crypto')
 
 //Global connection string
-var connection = mysql.createConnection({
+var con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Tumisang@07',
     database: 'contacts'
+})
+
+
+app.post("/contact",(req,res)=>{
+    var name = req.body.name
+    var type = req.body.type
+    var user_id = req.body.type
+
+    var query = "insert into contact values(?,?,?)"
+
+    con.query(query,[name,type,user_id],(err,rows,fields)=>{
+       if(err)
+       {
+           console.log("failed to add contact")
+           res.send(0)
+       }
+       console.log("added new contact")
+       res.send(1)
+    })
+
+})
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.listen(3000,()=>{
+    console.log("server is live on 3000")
 })
--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,6 +1,1 @@
 
-app.use(bodyParser.urlencoded({extended: false}))
-
-app.listen(3000,()=>{
-    console.log("server is live on 3000")
-})
