a9312134b785d514d361db41f00614ed85cbeba4
Tshweu <t.t.sephiri@gmail.com>
2019-11-25 17:17:58 +0200

    Different approach to App

---
+++ CList/app/src/main/java/com/example/clist/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.clist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    TextView personal,business, addB,addP;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        personal = (TextView) findViewById(R.id.Personal);
+        business = (TextView) findViewById(R.id.Business);
+        addB = (TextView) findViewById(R.id.AddBusiness);
+        addP = (TextView) findViewById(R.id.AddPersonal);
+
+        personal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Opening Personal contacts",Toast.LENGTH_SHORT)
+                        .show();
+
+                //creating personal contact intent
+                Intent p = new Intent(MainActivity.this,Personal.class);
+                startActivity(p);
+            }
+        });
+
+        business.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Opening business contacts",Toast.LENGTH_SHORT)
+                        .show();
+
+                //creating personal contact intent
+                Intent p = new Intent(MainActivity.this,Business.class);
+                startActivity(p);
+            }
+        });
+
+        addP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Opening add personal contacts",Toast.LENGTH_SHORT)
+                        .show();
+
+                //creating personal contact intent
+                Intent p = new Intent(MainActivity.this,AddPersonal.class);
+                startActivity(p);
+            }
+        });
+
+        addB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Opening add business contacts",Toast.LENGTH_SHORT)
+                        .show();
+
+                //creating personal contact intent
+                Intent p = new Intent(MainActivity.this,AddBusiness.class);
+                startActivity(p);
+            }
+        });
+    }
+
+}
---
+++ CList/app/src/main/java/com/example/clist/Retrofit/INodeJS.java
@@ -0,0 +1,79 @@
+package com.example.clist.Retrofit;
+
+import android.database.Observable;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface INodeJS {
+
+    @POST("/add/addPersonalContact")
+    @FormUrlEncoded
+    Observable<String> addPersonalContact(@Field("type")String type,
+                                          @Field("email")String email,
+                                          @Field("phone_number")String phone_number,
+                                          @Field("surname")String surname,
+                                          @Field("street1")String street1,
+                                          @Field("postal_code1")String postal_code1,
+                                          @Field("city1")String city1);
+
+    @POST("/add/addBusinessContact")
+    @FormUrlEncoded
+    Observable<String> addBusinessContact(@Field("type")String type,
+                                          @Field("emails")String email,
+                                          @Field("phone_numbers")String phone_number,
+                                          @Field("type_ad1")String type_ad1,
+                                          @Field("street1")String street1,
+                                          @Field("postal_code1")String postal_code1,
+                                          @Field("city1")String city1,
+                                          @Field("type_ad2")String type_ad2,
+                                          @Field("street2")String street2,
+                                          @Field("postal_code2")String postal_code2,
+                                          @Field("city2")String city2);
+
+    @POST("/delete/deletePersonalContact")
+    @FormUrlEncoded
+    Observable<String> deletePersonalContact(@Field("id") String id);
+
+    @POST("/delete/deleteBusinessContact")
+    @FormUrlEncoded
+    Observable<String> deleteBusinessContact(@Field("id") String id);
+
+    @POST("update/UpdatePersonalContact")
+    @FormUrlEncoded
+    Observable<String> updatePersonalContact(@Field("type")String type,
+                                             @Field("email")String email,
+                                             @Field("phone_number")String phone_number,
+                                             @Field("surname")String surname,
+                                             @Field("street1")String street1,
+                                             @Field("postal_code1")String postal_code1,
+                                             @Field("city1")String city1);
+
+    @POST("update/updateBusinessContact")
+    @FormUrlEncoded
+    Observable<String> updateBusinessContact(@Field("type")String type,
+                                             @Field("emails")String email,
+                                             @Field("phone_numbers")String phone_number,
+                                             @Field("type_ad1")String type_ad1,
+                                             @Field("street1")String street1,
+                                             @Field("postal_code1")String postal_code1,
+                                             @Field("city1")String city1,
+                                             @Field("type_ad2")String type_ad2,
+                                             @Field("street2")String street2,
+                                             @Field("postal_code2")String postal_code2,
+                                             @Field("city2")String city2);
+
+    @GET("get/getPersonalContact")
+    Call<List<PersonalContactModel>> getPersonalContacts();
+
+    @GET("get/getBusinessContact")
+    Call<List<BusinessContactModel>> getBusinessContacts();
+
+
+}
